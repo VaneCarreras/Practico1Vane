be420921d510a7b5df2d6c9e737a8a3487b34b17
VaneCarreras <vanecarreras91@gmail.com>
2024-06-06 15:38:20 -0300

    Subida

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practico1Vane.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
